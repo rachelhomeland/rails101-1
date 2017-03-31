441a69354378e5656b1f0b7849bf5a86d62edd68
rachel_homeland <rachel.zhuang81@gmail.com>
2017-03-31 18:34:09 +0800

    join & quit group action

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,7 @@ class User < ApplicationRecord
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
 
-  def is_member_of?(group)
-    participated_groups.include?(group)
-  end
+
 
   def join!(group)
   participated_groups << group
@@ -19,6 +17,10 @@ class User < ApplicationRecord
 
   def quit!(group)
   participated_groups.delete(group)
+  end
+
+  def is_member_of?(group)
+    participated_groups.include?(group)
   end
 
 end
